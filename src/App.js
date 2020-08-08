2879ee038dce8c2c0c7055f902392719357e9dc9
MFJRID <mfjr48@gmail.com>
2020-08-09 00:10:56 +0700

    Membuat Navbar dan File-file Halaman

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Pages/public/css/style.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Index from "./Pages/Index";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/" exact component={Index} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menus: [
+        { label: "Home", path: "/" },
+        { label: "About", path: "/about" },
+        { label: "Contact", path: "/contact" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.menus.map((menu) => (
+          <li className="navbar">
+            <Link to={menu.path}>{menu.label}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Navbar;
